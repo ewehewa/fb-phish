e9542b946de8fdcf78344b441f9c877b1051700e
Bensonate <bensonate98@gmail.com>
2025-05-28 11:12:11 -0700

    update

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,12 +1,9 @@
 import ErrorTypes from "../constants/errorTypes.js";
-import logger from "../util/logger.js";
 
 const errorHandler = (err, req, res, next)=>{
   let statusCode = err.statusCode || 500;
   let message = err.message || "Something went wrong";
   let errorType = err.errorType || ErrorTypes.SERVER_ERROR;
-
-  logger.error(`${req.method} ${req.originalUrl} - ${err.message} ${err.stack}`);
 
   return res.status(statusCode).json({
     success: false,
